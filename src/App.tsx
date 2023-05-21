da0b3a71fcc418a85afa938397268904ac443ee0
Bartek Tułodziecki <bartek.tulodziecki@gmail.com>
2023-05-21 21:59:29 +0200

    moved breweries state and api call to BreweriesPage

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,13 @@
-import React, {useEffect, useState} from 'react';
 import './App.css';
-import {Brewery} from "./models/Brewery";
 import {Route, Routes} from "react-router-dom";
 import {BreweriesPage} from "./pages/BreweriesPage";
 import {BreweryDetailsPage} from "./pages/BreweryDetailsPage";
 
 function App() {
-    const [
-        breweries,
-        setBreweries
-    ] = useState<[Brewery] | null>(null);
-
-    const getBreweries = async (text: string) => {
-        const result = await fetch(
-            text.length > 0 ?
-                `https://api.openbrewerydb.org/v1/breweries/search?query=${encodeURIComponent(text)}` :
-                'https://api.openbrewerydb.org/v1/breweries');
-        const jsonResult = await result.json();
-        setBreweries(jsonResult);
-    }
-
-    useEffect(() => {
-        getBreweries('').catch();
-    }, []);
-
     return (
         <>
             <Routes>
-                <Route path='/' element={<BreweriesPage breweries={breweries} getBreweries={getBreweries}/>}/>
+                <Route path='/' element={<BreweriesPage/>}/>
                 <Route path='brewery_details/:id' element={<BreweryDetailsPage/>}/>
             </Routes>
         </>
--- src/pages/BreweriesPage.tsx
+++ src/pages/BreweriesPage.tsx
@@ -2,13 +2,26 @@ import {BreweriesTable} from "../components/BreweriesTable";
 import {Brewery} from "../models/Brewery";
 import {NoResults} from "../components/NoResults";
 import {SearchBar} from "../components/SearchBar";
-import React from "react";
+import React, {useEffect, useState} from "react";
 
-interface BreweriesPageProps {
-    breweries?: [Brewery] | null
-    getBreweries: (text: string) => Promise<void>
-}
-export function BreweriesPage({breweries, getBreweries}: BreweriesPageProps) {
+export function BreweriesPage() {
+    const [
+        breweries,
+        setBreweries
+    ] = useState<[Brewery] | null>(null);
+
+    const getBreweries = async (text: string) => {
+        const result = await fetch(
+            text.length > 0 ?
+                `https://api.openbrewerydb.org/v1/breweries/search?query=${encodeURIComponent(text)}` :
+                'https://api.openbrewerydb.org/v1/breweries');
+        const jsonResult = await result.json();
+        setBreweries(jsonResult);
+    }
+
+    useEffect(() => {
+        getBreweries('').catch();
+    }, []);
     return (
         <>
             <SearchBar getBreweries={getBreweries}/>
